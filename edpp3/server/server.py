4d1eb977fd78a7e5d36562215e548c2a63e04000
William Zhang <wzhang3@travelers.com>
2025-07-09 16:13:50 -0400

    refactor

--- edpp3/server/server.py
+++ edpp3/server/server.py
@@ -12,23 +12,7 @@ with open(model_filename, 'rb') as file:
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    features = ['homeworld_Alderaan', 'homeworld_Aleen Minor', 'homeworld_Bestine IV',
-       'homeworld_Cerea', 'homeworld_Champala', 'homeworld_Chandrila',
-       'homeworld_Concord Dawn', 'homeworld_Corellia', 'homeworld_Dagobah',
-       'homeworld_Dathomir', 'homeworld_Dorin', 'homeworld_Eriadu',
-       'homeworld_Glee Anselm', 'homeworld_Haruun Kal', 'homeworld_Iktotch',
-       'homeworld_Iridonia', 'homeworld_Kalee', 'homeworld_Kashyyyk',
-       'homeworld_Malastare', 'homeworld_Mirial', 'homeworld_Mon Cala',
-       'homeworld_Muunilinst', 'homeworld_Naboo', 'homeworld_Ojom',
-       'homeworld_Quermia', 'homeworld_Rodia', 'homeworld_Ryloth',
-       'homeworld_Serenno', 'homeworld_Shili', 'homeworld_Skako',
-       'homeworld_Socorro', 'homeworld_Stewjon', 'homeworld_Sullust',
-       'homeworld_Tatooine', 'homeworld_Tholoth', 'homeworld_Toydaria',
-       'homeworld_Trandosha', 'homeworld_Troiken', 'homeworld_Tund',
-       'homeworld_Umbara', 'homeworld_Vulpter', 'homeworld_Zolan',
-       'unit_type_at-at', 'unit_type_at-st', 'unit_type_resistance_soldier',
-       'unit_type_stormtrooper', 'unit_type_tie_fighter',
-       'unit_type_tie_silencer', 'unit_type_unknown', 'unit_type_x-wing']
+    features = loaded_model.feature_names_in_
 
     if request.is_json:
         feature_dict = {x: 0 for x in features}
@@ -49,8 +33,6 @@ def predict():
         data_df = pd.DataFrame([feature_dict])
         result = loaded_model.predict(data_df)
 
-        # Convert result to a serializable Python type (e.g., int or list)
-        # Assuming result is an array-like object (e.g., numpy array)
         result = result.tolist()  # Convert to a list
 
         return jsonify({"prediction": result[0]})  # Return the result as a JSON response
